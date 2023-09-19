c06f406f7ca77a876f20237f37d2525ae41bd3da
pointm <2765211704@qq.com>
2023-09-19 09:15:38 +0800

    一点尝试

---
+++ alittleattempt.py
@@ -0,0 +1,13 @@
+class Father():
+    def __init__(self):
+        self.hw()
+        return
+    def hw(self):
+        print('HalloWorld')
+
+class Children(Father):
+    def hw(self):
+        print('No')
+
+f = Father()
+c = Children()
