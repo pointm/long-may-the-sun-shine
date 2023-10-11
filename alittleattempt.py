5bc1dbb37ed69bbcbcd6d5f6695f903f5e956ed0
pointm <2765211704@qq.com>
2023-10-11 12:01:38 +0800

    以后就在这里画图力

--- alittleattempt.py
+++ alittleattempt.py
@@ -1,14 +1,25 @@
+from math import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 class Father():
     def __init__(self):
         self.hw()
         return
+
     def hw(self):
         print('HalloWorld')
+
 
 class Children(Father):
     def hw(self):
         print('No')
 
-f = Father()
-c = Children()
-c.__init__()
+
+# f = Father()
+# c = Children()
+# c.__init__()
+
+if __name__ == '__main__':
+    print('HalloWorld')
