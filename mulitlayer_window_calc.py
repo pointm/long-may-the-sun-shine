8d24a125313506df32ac851654c281712ed77892
pointm <2765211704@qq.com>
2024-04-09 17:20:14 +0800

    终于能量守恒了，但是还是不对劲

--- mulitlayer_window_calc.py
+++ mulitlayer_window_calc.py
@@ -1,8 +1,8 @@
 # 2024.4.2 当前使用python版本3.12.2
 # 当前公式引用文献DOI:10.1007/s10762-007-9207-y
 import numpy as np
-from math import pi
-from numpy import exp, sqrt
+from math import pi, log10
+from numpy import exp, sqrt, abs
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
@@ -64,20 +64,25 @@ def transfer_matrix_p(
 
 
 if __name__ == "__main__":
-    freqlist = np.linspace(25, 45, 1000, endpoint=True) * 10**9
+    freqlist = np.linspace(25, 45, 2500, endpoint=True) * 10**9
+    # freq = 35 * 10**9
     a = 7.11 * 10**-3
     b = 3.555 * 10**-3
     m = 1
     n = 0
     epsilon = [1, 2.7, 9, 2.7, 1]
+    # epsilon = [1, 1, 1, 1, 1] # 测试用，看看匀质情况下传输矩阵对不对
     N = len(epsilon)
     miu = np.ones(N)
-    thickness = [9, 1.35 * 10**-3, 0.75 * 10**-3, 1.35 * 10**-3, 9]
+    thickness = [99, 1.35 * 10**-3, 0.75 * 10**-3, 1.35 * 10**-3, 99]
+    # thickness = [99, 1.12 * 10**-3, 0.52 * 10**-3, 1.12 * 10**-3, 99]
 
     k_cutoff = kc(
         a, b, m, n
     )  # 计算TE10模式下的波导截止波数，并不是截止频率，截至频率要用c*kc/(2*pi)
-    M11list = []
+    s11list = []
+    s21list = []
+    convlist = []
     for freq in freqlist:
         M_total = np.array([[1, 0], [0, 1]])  # 传输矩阵初始化
         kps = []  # 计算每个介质下的波数
@@ -90,10 +95,23 @@ if __name__ == "__main__":
                 ),
                 M_total,
             )
+        M11 = M_total[0, 0]
+        M12 = M_total[0, 1]
         M21 = M_total[1, 0]
         M22 = M_total[1, 1]
-        M11list.append(10 * np.log10(np.abs(M21 / M22) ** 2))
+        R = abs(M21 / M22) ** 2
+        T = abs((M11 * M22 - M12 * M21) / M22) ** 2
+        TOTAL = R + T  # 你到底守不守恒?
 
-    plt.plot(freqlist, M11list)
+        S11 = 10 * log10(R)
+        S21 = 10 * log10(T)
+        conv = 10 * log10(TOTAL)
+        s11list.append(S11)
+        s21list.append(S21)
+        convlist.append(conv)
+    plt.plot(freqlist, s11list, label="S11")
+    plt.plot(freqlist, s21list, label="S21")
+    plt.plot(freqlist, convlist, label="Convergence")
+    plt.legend()
     plt.show()
 pass
