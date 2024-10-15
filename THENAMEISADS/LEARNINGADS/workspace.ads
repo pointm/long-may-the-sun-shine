b29598007c5d6905e979fbcdebb91a8d31f898a4
pointm <2765211704@qq.com>
2024-10-15 16:27:32 +0800

    更新一下创建子网络的教程

---
+++ THENAMEISADS/LEARNINGADS/workspace.ads
@@ -0,0 +1,33 @@
+<ADSWorkspace Revision="3" Version="100">
+    <Workspace Name="">
+        <LibraryDefs Name="lib.defs" />
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_builtin" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_pelib" />
+        <Library Name="ads_serdes_ref_channel" />
+        <Library Name="ads_quantum" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <Library Name="LEARNINGADS_lib" />
+        <Cell Name="LEARNINGADS_lib:Design1" />
+        <Cell Name="LEARNINGADS_lib:Layout1" />
+        <Log Name="search_history.log" />
+    </Workspace>
+</ADSWorkspace>
