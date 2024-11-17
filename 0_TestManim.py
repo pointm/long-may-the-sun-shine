9d3b969126046a3d99d40ff8db43c47a619db856
pointm <2765211704@qq.com>
2024-11-17 22:00:49 +0800

    进一步测试，应该没什么问题

--- 0_TestManim.py
+++ 0_TestManim.py
@@ -6,3 +6,38 @@ class CreateCircle(Scene):
         circle = Circle()  # create a circle
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
+
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, RIGHT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
+
+
+from manim import *
+
+
+class MatchingEquationParts(Scene):
+    def construct(self):
+        variables = (
+            VGroup(MathTex("a"), MathTex("b"), MathTex("c"))
+            .arrange_submobjects()
+            .shift(UP)
+        )
+
+        eq1 = MathTex("{{x}}^2", "+", "{{y}}^2", "=", "{{z}}^2")
+        eq2 = MathTex("{{a}}^2", "+", "{{b}}^2", "=", "{{c}}^2")
+        eq3 = MathTex("{{a}}^2", "=", "{{c}}^2", "-", "{{b}}^2")
+
+        self.add(eq1)
+        self.wait(0.5)
+        self.play(TransformMatchingTex(Group(eq1, variables), eq2))
+        self.wait(0.5)
+        self.play(TransformMatchingTex(eq2, eq3))
+        self.wait(0.5)
